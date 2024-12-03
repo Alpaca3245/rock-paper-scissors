1a1c55dd2c28faceca4a3431f6b9c26cd12f9bc6
Alpaca3245 <kyle.luu11@gmail.com>
2024-12-03 02:20:15 -0500

    Finished base game

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,5 +1,23 @@
-humanScore = 0;
-computerScore = 0;
+let rounds = 0;
+const MAXROUNDS = 10;
+setRoundDisplay(0, MAXROUNDS);
+
+const rockBtn = document.getElementById("rock-btn");
+const paperBtn = document.getElementById("paper-btn");
+const scissorsBtn = document.getElementById("scissors-btn");
+const newGameBtn = document.getElementById("new-game-btn");
+const computerScoreElem = document.getElementById("computer-score");
+const humanScoreElem = document.getElementById("human-score");
+const displayComputerChoice = document.getElementById("display-computer-choice")
+const displayHumanChoice = document.getElementById("display-human-choice")
+const info = document.getElementById("info-text");
+
+let computerScore="0";
+let humanScore="0";
+
+rockBtn.disabled = true;
+paperBtn.disabled = true;
+scissorsBtn.disabled = true;
 
 // Returns a random choice of rock, paper or scissors as a string
 function getComputerChoice() {
@@ -27,63 +45,130 @@ function getComputerChoice() {
 
 // Plays a single round of rock paper scissors with the computer
 // Use play game to play several rounds
-function playRound(humanChoice, computerChoice) {
-    humanResult = humanChoice.toLowerCase();
-    computerResult = computerChoice.toLowerCase();
+function playGame(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    computerChoice = computerChoice.toLowerCase();
 
     if (humanChoice == computerChoice) {
         console.log("Tie!");
+        displayHumanChoice.innerText = capitalizeFirstLetter(humanChoice);
+        displayComputerChoice.innerText = capitalizeFirstLetter(computerChoice);
+        info.innerText = "Tie!"
     } else if ((humanChoice == "rock") && (computerChoice == "paper")) {
         console.log ("Computer wins!");
+        info.innerText = "Computer wins!"
+        displayHumanChoice.innerText = "Rock";
+        displayComputerChoice.innerText = "Paper";
+        computerScoreElem.innerText = ++computerScore;
     } else if ((humanChoice == "rock") && (computerChoice == "scissors")) {
         console.log ("Human wins!");
+        info.innerText = "Human wins!";
+        displayHumanChoice.innerText = "Rock";
+        displayComputerChoice.innerText = "Scissors";
+        humanScoreElem.innerText = ++humanScore;
     } else if ((humanChoice == "paper") && (computerChoice == "scissors")) {
         console.log ("Computer wins!");
+        info.innerText = "Computer wins!";
+        displayHumanChoice.innerText = "Paper";
+        displayComputerChoice.innerText = "Scissors";
+        computerScoreElem.innerText = ++computerScore;
     } else if ((humanChoice == "paper") && (computerChoice == "rock")) {
         console.log ("Human wins!");
+        info.innerText ="Human wins!";
+        displayHumanChoice.innerText = "Paper";
+        displayComputerChoice.innerText = "Rock";
+        humanScoreElem.innerText = ++humanScore;
     } else if ((humanChoice == "scissors") && (computerChoice == "rock")) {
         console.log ("Computer wins!");
+        info.innerText = "Computer wins!";
+        displayHumanChoice.innerText = "Scissors";
+        displayComputerChoice.innerText = "Rock";
+        computerScoreElem.innerText = ++computerScore;
     } else if ((humanChoice == "scissors") && (computerChoice == "paper")) {
         console.log ("Human wins!");
+        info.innerText = "Human wins!";
+        displayHumanChoice.innerText = "Scissors";
+        displayComputerChoice.innerText = "Paper";
+        humanScoreElem.innerText = ++humanScore;
     } else {
         console.log ("Input error!");
+    }
+    rounds++;
+
+    setRoundDisplay(rounds,MAXROUNDS);
+
+    // Copied from the internet
+    function capitalizeFirstLetter(val) {
+        return String(val).charAt(0).toUpperCase() + String(val).slice(1);
+    }
+    
+    // Ends games 
+    if (rounds == MAXROUNDS) {
+        // Disable the buttons for playing
+        rockBtn.disabled = true;
+        paperBtn.disabled = true;
+        scissorsBtn.disabled = true;
+        newGameBtn.disabled = false;
+
+        rounds = 0;
     }
     return 0;
 }
 
-function playGame(humanBtnSelection) { // Runs inputs arguments for playRound and starts playRound function several times (start rock paper scissors game)
-    const maxRounds = 5; // Number of rounds
-
-    for (let i = 0; i < maxRounds; i++) {
-        const humanSelection = humanBtnSelection();
-        const computerSelection = getComputerChoice();
-    
-        playRound(humanSelection, computerSelection)
-        console.log("----------------------------");
-    }
+function resetScore() {
+    computerScoreElem.innerText = 0;
+    computerScore = 0;
+    humanScoreElem.innerText = 0;
+    humanScore = 0;
 }
 
 
-// Buttons events
+function setRoundDisplay(rounds, MAXROUNDS) {
+    const numberOfRounds = document.getElementById("number-of-rounds");
+    numberOfRounds.innerText = `${rounds}/${MAXROUNDS}`
+}
+
+function newGame() {
+    // Enables the buttons and disables newgame
+    rockBtn.disabled = false;
+    paperBtn.disabled = false;
+    scissorsBtn.disabled = false;
+    newGameBtn.disabled = true;
+
+    info.innerText = "Placeholder";
+    displayHumanChoice.innerText = "None";
+    displayComputerChoice.innerText = "None";
+
+    resetScore();
+    setRoundDisplay(0,MAXROUNDS);
+}
+
+
+// Events
+newGameBtn.addEventListener("click", newGame)
+
 const btnGroup = document.querySelector(".game-controls");
 
 btnGroup.addEventListener("click", e => {
     console.log(e.target);
     let targetText = e.target.textContent; // Assigning to variable is easier and prevents the system from finding the variable several times
-    let humanBtnSelection = "";
+    let humanBtnChoice = "";
 
     switch(targetText) {
         case 'Rock':
             console.log("Rock");
-            humanBtnSelection = targetText;
+            humanBtnChoice = targetText;
+            playGame(humanBtnChoice, getComputerChoice());
             break;
         case 'Paper':
             console.log("Paper");
-            humanBtnSelection = targetText;
+            humanBtnChoice = targetText;
+            playGame(humanBtnChoice, getComputerChoice());
         break;     
         case 'Scissors':
             console.log("Scissors");
-            humanBtnSelection = targetText;
+            humanBtnChoice = targetText;
+            playGame(humanBtnChoice, getComputerChoice());
         break;       
     }
 })
